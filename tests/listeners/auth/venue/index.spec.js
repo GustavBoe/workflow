df0d29480d2fac0370e400ad1a4eb4fa0b3889b3
GustavBoe <gustavhugoboe@icloud.com>
2025-10-15 22:07:35 +0200

    Added credit for specifier in test

--- tests/listeners/auth/venue/index.spec.js
+++ tests/listeners/auth/venue/index.spec.js
@@ -5,8 +5,10 @@ test.describe("Venue", () => {
     page,
   }) => {
     await page.goto("/");
+    // Specifier inspired by github users and fellow students: Jereriviel and martir2109
     await page.waitForSelector("#venue-container a");
     await page.locator("#venue-container a").first().click();
+    //
     await expect(page.locator("h1")).toContainText("Venue details");
   });
 });
